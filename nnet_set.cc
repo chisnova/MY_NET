7a66d6c4dfa36d25b37108a2cbc66a8ee0e12108
chisnova <chisnova@naver.com>
2018-02-20 09:19:12 +0000

    Insert Layer_type_2D data structure

--- nnet_set.cc
+++ nnet_set.cc
@@ -121,6 +121,14 @@ class type_2D
 			printf("\n");
 		}
 	}
+	void zero_err()
+	{
+		for(int i=0;i<row;++i){
+			for(int j=0;j<col;++j){
+				err[i][j]=0;
+			}
+		}
+	}
 
 	~type_2D()
 	{
@@ -129,48 +137,51 @@ class type_2D
 	}
 };
 
-class type_3D
+class Layer_type_2D
 {
 	public:
-		type_2D* image;
 		int row;
 		int col;
 		int size;
-	type_3D(int i_size,int i_row,int i_col)
+		type_2D* image[];
+
+	Layer_type_2D(int i_size,int i_row,int i_col)
 	{
-		size=i_idx
+		size=i_size;
 		row=i_row;
 		col=i_col;
-		type_2D* image[size];
 		for(int i=0;i<size;++i) image[i] = new type_2D(row,col);
 	}
 
-	void show_val(int i_idx)
+	void show_val(int idx)
 	{
 		for(int i=0;i<row;++i){
 			for(int j=0;j<col;++j){
-				printf("%lf ",image[i_idx]->val[i][j]);
+				printf("%lf ",image[idx]->val[i][j]);
 			}
 			printf("\n");
 		}
 	}
 
-	void show_err(int i_idx)
+	void show_err(int idx)
 	{
 		for(int i=0;i<row;++i){
 			for(int j=0;j<col;++j){
-				printf("%lf ",image[i_idx]->err[i][j]);
+				printf("%lf ",image[idx]->err[i][j]);
 			}
 			printf("\n");
 		}
 	}
-
-	~type_3D()
+	void zero_err(int idx)
 	{
-		for(int i=0;i<idx;++i) delete image[i];
+		image[idx]->zero_err();
+	}
+
+	~Layer_type_2D()
+	{
+		for(int i=0;i<size;++i) delete image[i];
 	}
 };
-
 
 class Layer
 {
@@ -552,8 +563,8 @@ class Layer
 				}
 			}
 		}
-		//3D
-		void sigmoid(type_3D* in,type_3D* out)
+		//Layer 2D
+		void sigmoid(Layer_type_2D* in,Layer_type_2D* out)
 		{
 			double x;
 			for(int k=0;k<in->size;++k){
@@ -565,7 +576,7 @@ class Layer
 				}
 			}
 		}
-		void sigmoid(type_3D* in,type_3D* out,double lr)
+		void sigmoid(Layer_type_2D* in,Layer_type_2D* out,double lr)
 		{
 			double y;
 			double err;
@@ -580,7 +591,7 @@ class Layer
 			}
 		}
 
-		void relu(type_3D* in,type_3D* out)
+		void relu(Layer_type_2D* in,Layer_type_2D* out)
 		{
 			double x;
 			for(int k=0;k<in->size;++k){
@@ -593,34 +604,34 @@ class Layer
 				}
 			}
 		}
-		void relu(type_3D* in,type_3D* out,double lr)
+		void relu(Layer_type_2D* in,Layer_type_2D* out,double lr)
 		{
 			double err;
 			for(int k=0;k<in->size;++k){
 				for(int i=0;i<in->row;++i){
 					for(int j=0;j<in->col;++j){
 						err=in->image[k]->err[i][j];
-						if(in->val[i][j]<=0) in->image[i]->err[i][j]=0;
+						if(in->image[k]->val[i][j]<=0) in->image[i]->err[i][j]=0;
 						else in->image[k]->err[i][j]=err;
 					}
 				}
 			}
 		}
 
-		void tanh(type_3D* in,type_3D* out)
+		void tanh(Layer_type_2D* in,Layer_type_2D* out)
 		{
 			double x;
 			for(int k=0;k<in->size;++k){
 				for(int i=0;i<in->row;++i){
 					for(int j=0;j<in->col;++j){
 						x=in->image[k]->val[i][j];
-						out->val[i][j]=(exp(2*x-1)/(exp(2*x)+1);	
+						out->image[k]->val[i][j]=(exp(2*x-1)/(exp(2*x)+1));	
 					}
 				}
 			}
 		}
 
-		void tanh(type_3D* in,type_3D* out,double lr)
+		void tanh(Layer_type_2D* in,Layer_type_2D* out,double lr)
 		{
 			double err;
 			double y;
@@ -634,7 +645,7 @@ class Layer
 				}
 			}
 		}
-		void softmax(type_3D* in,type_3D* out)
+		void softmax(Layer_type_2D* in,Layer_type_2D* out)
 		{
 			double sum;
 			double x;
@@ -653,22 +664,22 @@ class Layer
 			}
 		}
 
-		void softmax(type_3D* in,type_3D* out,double lr)
+		void softmax(Layer_type_2D* in,Layer_type_2D* out,double lr)
 		{
 			double yi;
 			double yj;
 			double err;
-			for(int k=0;k<in->size;++k){
+			for(int l=0;l<in->size;++l){
 				for(int i=0;i<in->row;++i){
 					for(int j=0;j<in->col;++j){
-						yi=out->image[k]->val[i][j];
-						in->image[k]->err[i][j]=0;
+						yi=out->image[l]->val[i][j];
+						in->image[l]->err[i][j]=0;
 						for(int k=0;k<in->row;++k){
 							for(int n=0;n<in->col;++n){
-								yj=out->image[k]->val[k][n];
-								err=out->image[k]->err[k][n];
-								if(i==k && j ==n) in->image[k]->err[i][j]+=yi*(1-yi)*err;	
-								else in->image[k]->err[i][j]+=-yi*yj*err;	
+								yj=out->image[l]->val[k][n];
+								err=out->image[l]->err[k][n];
+								if(i==k && j ==n) in->image[l]->err[i][j]+=yi*(1-yi)*err;	
+								else in->image[l]->err[i][j]+=-yi*yj*err;	
 							}
 						}
 					}
@@ -676,176 +687,189 @@ class Layer
 			}
 		}
 
-		void conv(type_3D* in,type_3D* kernel,type_3D* out)
+		void conv(Layer_type_2D* in,Layer_type_2D* kernel,Layer_type_2D* out)
 		{
 			int i_width=in->col;
 			int i_depth=in->row;
-			int i_depth=in->row;
+			int i_size=in->size;
 			int k_width=kernel->col;
 			int k_depth=kernel->row;
 			int k_size=(k_width)*(k_depth);
 
-			for(int i=0;i<i_depth;++i){
-				for(int j=0;j<i_width;++j){
-					if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
-					double sum=0;
-					for(int k=0;k<k_size;++k){
-						int dy=k/k_width;
-						int dx=k%k_width;
-						sum+=(in->val[i+dy][j+dx])*(kernel->val[dy][dx]);	
+			for(int m=0;m<kernel->size;++m){
+				for(int n=0;n<in->size;++n){
+					for(int i=0;i<i_depth;++i){
+						for(int j=0;j<i_width;++j){
+							if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
+							double sum=0;
+							for(int k=0;k<k_size;++k){
+								int dy=k/k_width;
+								int dx=k%k_width;
+								sum+=(in->image[n]->val[i+dy][j+dx])*(kernel->image[m]->val[dy][dx]);	
+							}
+							if(n==0) out->image[m]->val[i][j]=sum;	
+							else out->image[m]->val[i][j]+=sum;	
+						}	
 					}
-					if(acc==0) out->val[i][j]=sum;	
-					else out->val[i][j]+=sum;	
-				}	
+				}
 			}
 		}
 
-		void conv(type_2D* in,type_2D* kernel,type_2D* out,int acc,double lr)
+		void conv(Layer_type_2D* in,Layer_type_2D* kernel,Layer_type_2D* out,double lr)
 		{
 			int i_width=in->col;
 			int i_depth=in->row;
+			int i_size=in->size;
 			int k_width=kernel->col;
 			int k_depth=kernel->row;
 			int k_size=(k_width)*(k_depth);
-		
-			if(acc==0){
-				for(int i=0;i<i_depth;++i){
-		   			for(int j=0;j<i_width;++j){
-		   				in->err[i][j]=0;
-		   			}
-		   		}
-			}
-		   	for(int i=0;i<i_depth;++i){
-		   		for(int j=0;j<i_width;++j){
-		   			if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
-		   			for(int k=0;k<k_size;++k){
-		   				int dy=k/k_width;
-		   				int dx=k%k_width;
-		   				in->err[i+dy][j+dx]+=(kernel->val[dy][dx])*(out->err[i][j]);	
-		   			}
-		   		}	
-		   }
-		}
+			int dy,dx;	
+			double err;
+			double k_value;
 
-		void conv_update(type_2D* in,type_2D* kernel,type_2D* out,int acc,double lr)
-		{
-			int i_width=in->col;
-			int i_depth=in->row;
-			int k_width=kernel->col;
-			int k_depth=kernel->row;
-			int k_size=(k_width)*(k_depth);
-		
-			double update=0;
-			for(int i=0;i<i_depth;++i){
-		   		for(int j=0;j<i_width;++j){
-		   			for(int k=0;k<k_size;++k){
-		   				int dy=k/k_width;
-		   				int dx=k%k_width;
-		   				if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
-		   				update=(in->val[i+dy][j+dx])*(out->err[i][j]);	
-						kernel->val[dy][dx]-=update*lr;
-		   			}
-		   		}	
+			for(int n=0;n<in->size;++n){
+				in->image[n]->zero_err();
+				for(int m=0;m<kernel->size;++m){
+		   			for(int i=0;i<i_depth;++i){
+		   				for(int j=0;j<i_width;++j){
+		   					if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
+		   					for(int k=0;k<k_size;++k){
+		   						dy=k/k_width;
+		   						dx=k%k_width;
+								k_value=kernel->image[m]->val[dy][dx];
+								err=out->image[m]->err[i][j];
+								in->image[n]->err[i+dy][j+dx]+=k_value*err;	
+		   					}
+		   				}	
+		    		}
+				}
 			}
-		}
 
-		void max_pooling(type_2D* in,type_2D* out,int poolsize)
-		{
-			int i_width=in->col;
-			int i_depth=in->row;
-			int k_width=poolsize;
-			int k_depth=poolsize;
-			int k_size=(k_width)*(k_depth);
-			for(int i=0;i<i_depth;i+=poolsize){
-				for(int j=0;j<i_width;j+=poolsize){
-					double max=-987654321;
-					if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
-					double sum=0;
-					for(int k=0;k<k_size;++k){
-						int dx=k/k_width;
-						int dy=k%k_width;
-						if(in->val[i+dx][j+dy]>max) max=in->val[i+dx][j+dy];
+			double update;
+			for(int m=0;m<kernel->size;++m){
+				for(int n=0;n<in->size;++n){
+					for(int i=0;i<i_depth;++i){
+		   				for(int j=0;j<i_width;++j){
+		   					for(int k=0;k<k_size;++k){
+		   						dy=k/k_width;
+		   						dx=k%k_width;
+		   						if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
+		   						update=(in->image[n]->val[i+dy][j+dx])*(out->image[m]->err[i][j]);	
+								kernel->image[m]->val[dy][dx]-=update*lr;
+		   					}
+		   				}	
 					}
-					out->val[i/poolsize][j/poolsize]=max;	
 				}
 			}
 		}
 
-		void max_pooling(type_2D* in,type_2D* out,int poolsize,double lr)
+		void max_pooling(Layer_type_2D* in,Layer_type_2D* out,int poolsize)
 		{
 			int i_width=in->col;
 			int i_depth=in->row;
 			int k_width=poolsize;
 			int k_depth=poolsize;
 			int k_size=(k_width)*(k_depth);
-			for(int i=0;i<i_depth;i+=poolsize){
-				for(int j=0;j<i_width;j+=poolsize){
-					if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
-					int max_x,max_y;
-					double max=-987654321;
-					for(int k=0;k<k_size;++k){
-						int dx=k/k_width;
-						int dy=k%k_width;
-						if(in->val[i+dx][j+dy]>max){
-							max_x=i+dx;
-							max_y=j+dy;
-							max=in->val[i+dx][j+dy];
+			double max;
+			int dy,dx;
+
+			for(int n=0;n<in->size;++n){
+				for(int i=0;i<i_depth;i+=poolsize){
+					for(int j=0;j<i_width;j+=poolsize){
+						max=-987654321;
+						if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
+						for(int k=0;k<k_size;++k){
+							dy=k/k_width;
+							dx=k%k_width;
+							if(in->image[n]->val[i+dy][j+dx]>max) max=in->image[n]->val[i+dy][j+dx];
+						}
+						out->image[n]->val[i/poolsize][j/poolsize]=max;	
+					}
+				}
+			}
+		}
+
+		void max_pooling(Layer_type_2D* in,Layer_type_2D* out,int poolsize,double lr)
+		{
+			int i_width=in->col;
+			int i_depth=in->row;
+			int k_width=poolsize;
+			int k_depth=poolsize;
+			int k_size=(k_width)*(k_depth);
+			int dy,dx;
+			int max_x,max_y;
+			double max;
+
+			for(int n=0;n<in->size;++n){
+				in->image[n]->zero_err();
+				for(int i=0;i<i_depth;i+=poolsize){
+					for(int j=0;j<i_width;j+=poolsize){
+						if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
+						max=-987654321;
+						for(int k=0;k<k_size;++k){
+							dy=k/k_width;
+							dx=k%k_width;
+							if(in->image[n]->val[i+dy][j+dx]>max){
+								max_y=i+dy;
+								max_x=j+dx;
+								max=in->image[n]->val[i+dy][j+dx];
+							}
+						}
+						in->image[n]->err[max_y][max_x]=out->image[n]->err[i/poolsize][j/poolsize];
+					}
+				}
+			}
+			
+		}
+
+		void average_pooling(Layer_type_2D* in,Layer_type_2D* out,int poolsize)
+		{
+			int i_width=in->col;
+			int i_depth=in->row;
+			int k_width=poolsize;
+			int k_depth=poolsize;
+			int k_size=(k_width)*(k_depth);
+			double sum;
+			int dy,dx;
+
+			for(int n=0;n<in->size;++n){
+				for(int i=0;i<i_depth;i+=poolsize){
+					for(int j=0;j<i_width;j+=poolsize){
+						if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
+						sum=0;
+						for(int k=0;k<k_size;++k){
+							dy=k/k_width;
+							dx=k%k_width;
+							sum+=(in->image[n]->val[i+dy][j+dx]);	
+						}
+						out->image[n]->val[i/poolsize][j/poolsize]=sum/(double)k_size;	
+					}
+				}
+			}
+		}
+
+		void average_pooling(Layer_type_2D* in,Layer_type_2D* out,int poolsize,double lr)
+		{
+			int i_width=in->col;
+			int i_depth=in->row;
+			int k_width=poolsize;
+			int k_depth=poolsize;
+			int k_size=(k_width)*(k_depth);
+			int dy,dx;
+
+			for(int n=0;n<in->size;++n){
+				for(int i=0;i<i_depth;i+=poolsize){
+					for(int j=0;j<i_width;j+=poolsize){
+						if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
+						for(int k=0;k<k_size;++k){
+							dy=k/k_width;
+							dx=k%k_width;
+							in->image[n]->err[i+dy][j+dx]=(out->image[n]->err[i/poolsize][j/poolsize])/(double)k_size;
 						}
 					}
-					for(int k=0;k<k_size;++k){
-						int dx=k/k_width;
-						int dy=k%k_width;
-						if(max_x==(i+dx) && max_y==(j+dy)){
-							in->err[i+dx][j+dy]=out->err[i/poolsize][j/poolsize];
-						}
-						else{
-							in->err[i+dx][j+dy]=0;
-						}
-					}
 				}
 			}
 		}
-
-		void average_pooling(type_2D* in,type_2D* out,int poolsize)
-		{
-			int i_width=in->col;
-			int i_depth=in->row;
-			int k_width=poolsize;
-			int k_depth=poolsize;
-			int k_size=(k_width)*(k_depth);
-			for(int i=0;i<i_depth;i+=poolsize){
-				for(int j=0;j<i_width;j+=poolsize){
-					if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
-					double sum=0;
-					for(int k=0;k<k_size;++k){
-						int dx=k/k_width;
-						int dy=k%k_width;
-						sum+=(in->val[i+dx][j+dy]);	
-					}
-					out->val[i/poolsize][j/poolsize]=sum/(double)k_size;	
-				}
-			}
-		}
-
-		void average_pooling(type_2D* in,type_2D* out,int poolsize,double lr)
-		{
-			int i_width=in->col;
-			int i_depth=in->row;
-			int k_width=poolsize;
-			int k_depth=poolsize;
-			int k_size=(k_width)*(k_depth);
-			for(int i=0;i<i_depth;i+=poolsize){
-				for(int j=0;j<i_width;j+=poolsize){
-					if(!isRange(i+k_depth,i_depth,j+k_width,i_width)) break;
-					for(int k=0;k<k_size;++k){
-						int dx=k/k_width;
-						int dy=k%k_width;
-						in->err[i+dx][j+dy]=(out->err[i/poolsize][j/poolsize])/(double)k_size;
-					}
-				}
-			}
-		}
-
 };
 
 #define layer_size 5
@@ -877,7 +901,6 @@ void DNN()
 	int miss;
 	double mse;
 	while(1){
-
 		hit=0;
 		miss=0;
 		mse=0;
@@ -1060,11 +1083,118 @@ void LeNet()
 	}
 }
 
+void LeNet2()
+{
+	srand((unsigned int)time(NULL));
+	readMnist(&train_data,&test_data);
+
+	int tr_size=100;
+	int test_size=10;
+	double lr=0.001;
+
+    //conv,pool,conv,pool,conv//
+	Layer_type_2D* x0 = new Layer_type_2D(1,32,32);
+	Layer_type_2D* k0 = new Layer_type_2D(6,5,5);
+	Layer_type_2D* x1 = new Layer_type_2D(6,28,28);
+	Layer_type_2D* s1 = new Layer_type_2D(6,14,14);
+	Layer_type_2D* k1 = new Layer_type_2D(16,5,5);
+	Layer_type_2D* x2 = new Layer_type_2D(16,10,10);
+	Layer_type_2D* s2 = new Layer_type_2D(16,5,5);
+	Layer_type_2D* k2 = new Layer_type_2D(120,5,5);
+	Layer_type_2D* x3 = new Layer_type_2D(120,1,1);
+
+	type_1D* dx1 = new type_1D(120);
+	type_2D* dw1 = new type_2D(84,120);
+	type_1D* dx2 = new type_1D(84);
+	type_1D* da2 = new type_1D(84);
+	type_2D* dw2 = new type_2D(10,84);
+	type_1D* dx3 = new type_1D(10);
+	type_1D* da3 = new type_1D(10);
+
+	Layer test;
+	int hit;
+	int miss;
+	double mse;
+
+	while(1)
+	{
+		hit=0;
+		miss=0;
+		mse=0;
+		for(int tr=0;tr<tr_size;++tr){
+			//FORWARD
+			for(int i=0;i<32;++i){for(int j=0;j<32;++j){
+				x0->image[0]->val[i][j]=train_data.image[tr][i][j]*0.001;
+			}}
+
+			test.conv(x0,k0,x1);	
+			test.average_pooling(x1,s1,2);	
+			test.conv(s1,k1,x2);
+			test.average_pooling(x2,s2,2);
+			test.conv(s2,k2,x3);
+			for(int i=0;i<120;++i)	dx1->val[i]=x3->image[i]->val[0][0];
+			test.affine(dx1,dw1,dx2);
+			test.sigmoid(dx2,da2);
+			test.affine(da2,dw2,dx3);
+			test.sigmoid(dx3,da3);
+
+			if(da3->max_idx()==train_data.idx[tr]) hit++;
+			else miss++;
+			for(int i=0;i<10;++i){da3->err[i]=da3->val[i]-train_data.label[tr][i];}
+			for(int i=0;i<10;++i){mse+=(da3->err[i])*(da3->err[i]);}
+
+			//BACKWARD
+			test.sigmoid(dx3,da3,lr);
+			test.affine(da2,dw2,dx3,lr);
+			test.sigmoid(dx2,da2,lr);
+			test.affine(dx1,dw1,dx2,lr);
+			for(int i=0;i<120;++i) x3->image[i]->err[0][0]=dx1->err[i];
+			test.conv(s2,k2,x3,lr);
+			test.average_pooling(x2,s2,2,lr);
+			test.conv(s1,k1,x2,lr);
+			test.average_pooling(x1,s1,2,lr);	
+			test.conv(x0,k0,x1,lr);	
+		}
+		printf("Train Recognition Rate %lf\n",double(hit)/(double(hit)+double(miss))*100);
+		printf("Train MSE %lf\n",sqrt(mse/double(hit+miss)));
+
+		hit=0;
+		miss=0;
+		mse=0;
+		for(int tr=0;tr<test_size;++tr){
+			//FORWARD
+			for(int i=0;i<32;++i){for(int j=0;j<32;++j){
+				x0->image[0]->val[i][j]=test_data.image[tr][i][j]*0.001;
+			}}
+			
+			test.conv(x0,k0,x1);	
+			test.average_pooling(x1,s1,2);	
+			test.conv(s1,k1,x2);
+			test.average_pooling(x2,s2,2);
+			test.conv(s2,k2,x3);
+			for(int i=0;i<120;++i)	dx1->val[i]=x3->image[i]->val[0][0];
+			test.affine(dx1,dw1,dx2);
+			test.sigmoid(dx2,da2);
+			test.affine(da2,dw2,dx3);
+			test.sigmoid(dx3,da3);
+
+			if(da3->max_idx()==test_data.idx[tr]) hit++;
+			else miss++;
+
+			for(int i=0;i<10;++i){da3->err[i]=da3->val[i]-test_data.label[tr][i];}
+			for(int i=0;i<10;++i){mse+=(da3->err[i])*(da3->err[i]);}
+
+		}
+		printf("Test Recognition Rate %lf\n",double(hit)/(double(hit)+double(miss))*100);
+		printf("Test MSE %lf\n",sqrt(mse/double(hit+miss)));
+	}
+}
 
 int main()
 {
 	//DNN();	
-	LeNet();	
+	//LeNet();	
+	LeNet2();	
 	return 0;	
 }
 
